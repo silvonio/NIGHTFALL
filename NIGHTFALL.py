779b3056c871974713fcaf436a6f4e7356298acf
Antonio <antoniosanmu100@gmail.com>
2019-06-02 20:16:57 +0200

    Añadidas las imágenes de los huevos

--- NIGHTFALL.py
+++ NIGHTFALL.py
@@ -24,6 +24,9 @@ spacePressed = False
 spaceReleased = False
 huevera = [] # Para guardar todos los objetos huevo
 timeForEggs = 3000 # Para almacenar el instante en el que se ha creado el último huevo
+timeForTitle = GAME_TIME.get_ticks() # Para almecenar el instante del último parpadeo
+timeBetweenOff = random.randint(1500, 10000) # El tiempo entre cada parpadeo
+initTime = None # Para almacenar el instante de inicio del juego
 
 # CONSTANTS
 
@@ -31,6 +34,8 @@ WINDOW_WIDTH = 1000
 WINDOW_HEIGHT = 700
 FPS = 30
 TIMEBETWEENEGGS = 3000
+GAMEDURATION = 10000 # Los milisegundos que dura una partida
+LIGHTCHANGES = [(0, 0), (200, 1), (500, 0), (800, 1), (850, 0), (1000, 1), (1100, 0), (1300, 1)]
 
 # PYGAME OBJECTS
 
@@ -43,6 +48,11 @@ textFont = pygame.font.SysFont("arialblack", 30)
 huevo = egg.egg()
 
 # LOAD IMAGES
+
+titleImage = [
+    pygame.image.load("assets/images/background/off.png"),
+    pygame.image.load("assets/images/background/on.png")
+]
 
 # LOAD SOUNDS
 
@@ -57,14 +67,40 @@ def resetPressed():
     spacePressed = False
     spaceReleased = False
 
+def drawSun(posY, color):
+    global surface
+    pygame.draw.circle(surface, color, (int(WINDOW_WIDTH*0.7),posY), 50)
+
+def drawTitle():
+    global titleImage, timeForTitle, timeBetweenOff
+    time = GAME_TIME.get_ticks()-timeForTitle
+    if time <= timeBetweenOff:
+        for state in LIGHTCHANGES:
+            if time > state[0]:
+                imageToDraw = titleImage[state[1]]
+    else:
+        imageToDraw = titleImage[0]
+        timeForTitle = GAME_TIME.get_ticks()
+        timeBetweenOff = random.randint(1500, 10000)  # El tiempo entre cada parpadeo
+    surface.blit(imageToDraw, (100, 200))
+
+def map (x0, xf, y0, yf, x):
+    m = (yf-y0)/(xf-x0)
+    return int(m*(x-x0)+y0)
+
 def drawStage():
     global surface, state
-    surface.fill((0, 0, 0))
     if state == "menuScreen":
+        surface.fill((0, 0, 0))
+        drawTitle()
         renderedText = textFont.render('Pulsa espacio', 2, (255, 255, 255))
-        surface.blit(renderedText, (350, 350))
+        surface.blit(renderedText, (350, 400))
 
     elif state == "playing":
+        time = GAME_TIME.get_ticks() - initTime
+        surface.fill((map(0,GAMEDURATION,160,70,time), map(0,GAMEDURATION,220,0,time), map(0,GAMEDURATION,185,0,time)))
+        drawSun(map(0,GAMEDURATION,-50,WINDOW_HEIGHT+50, time),(255,map(0,GAMEDURATION,255,50,time),0))
+
         # Para dibujar los palos
 
         pygame.draw.rect(surface, (255, 0, 0), pygame.Rect(0, 0, 1000, 30))
@@ -117,6 +153,7 @@ while True:
         menuScreen()
         if spaceReleased:
             state = "playing"
+            initTime = GAME_TIME.get_ticks()
             resetPressed()
 
     elif state == "playing":
